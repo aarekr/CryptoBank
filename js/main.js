19408514c3e8e95728fa81ec6ee63998d603ebcc
aarekr <aare212@hotmail.com>
2025-04-28 08:25:36 +0300

    Mobile menu

--- js/main.js
+++ js/main.js
@@ -2,9 +2,13 @@
 
 document.addEventListener('DOMContentLoaded', () => {
     const faqContainer = document.querySelector('.faq-content');
+
     faqContainer.addEventListener('click', (e) => {
         const groupHeader = e.target.closest('.faq-group-header');
-        if(!groupHeader) return;
+
+        if(!groupHeader)
+            return;
+
         const group = groupHeader.parentElement;
         const groupBody = group.querySelector('.faq-group-body');
         const icon = groupHeader.querySelector('i');
@@ -18,14 +22,26 @@ document.addEventListener('DOMContentLoaded', () => {
 
         // Close other open FAQ bodies
         const otherGroups = faqContainer.querySelectorAll('.faq-group');
+
         otherGroups.forEach((otherGroup) => {
             if(otherGroup !== group) {
                 const otherGroupBody = otherGroup.querySelector('.faq-group-body');
                 const otherIcon = otherGroup.querySelector('.faq-group-header i');
+
                 otherGroupBody.classList.remove('open');
                 otherIcon.classList.remove('fa-minus');
                 otherIcon.classList.add('fa-plus');
             }
         });
     });
+});
+
+// Mobile Menu
+document.addEventListener('DOMContentLoaded', () => {
+    const hamburgerButton = document.querySelector('.hamgurger-button');
+    const mobileMenu = document.querySelector('.mobile-menu');
+
+    hamburgerButton.addEventListener('click', () => 
+        mobileMenu.classList.toggle('active')
+    );
 });
